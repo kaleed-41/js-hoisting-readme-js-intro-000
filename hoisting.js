5ca214f3e70875e108f713923713d6982f449565
Daniel Williams <danielwil621@gmail.com>
2021-04-01 02:09:05 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -18,11 +18,11 @@ var test = thisIsCrazy();
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-
-  sayMy();
-
+  
   function sayMy() {
     console.log(name);
     var name = "Kristin";
   }
 }
+var scope = sayMyName();
+scope();
